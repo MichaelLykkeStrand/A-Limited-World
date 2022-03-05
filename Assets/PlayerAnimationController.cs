4996418358805534d5b106e08ed31104a3d4ce02
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2022-03-05 23:42:22 +0100

    Player anim idle

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerAnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 prevPos, velocity;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    [SerializeField] float xDeadzone = 0.1f;
+    private void Awake()
     {
-        
+        prevPos = transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = (transform.position - prevPos) / Time.deltaTime;
+        prevPos = transform.position;
+        if (velocity.x < -xDeadzone)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(velocity.x > xDeadzone)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        animator.SetFloat("Velocity", velocity.magnitude);
     }
 }
