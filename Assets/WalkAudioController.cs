7414b91e68aac3896a8a978022267c98c61c532d
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2022-03-05 22:33:48 +0100

    Player walk sound

---
+++ Assets/WalkAudioController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class WalkAudioController : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> audioClips = new List<AudioClip>();
+    AudioSource audioSource;
+    Vector3 pos, velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        pos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        velocity = (transform.position - pos) / Time.deltaTime;
+        pos = transform.position;
+
+        if(Mathf.Abs(velocity.magnitude) <= 0 || audioSource.isPlaying) return;
+        var clip = audioClips[Random.Range(0,audioClips.Count)];
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
